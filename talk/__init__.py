1028d9967cdb93cde14bd6bc975a0d17ebf7eb06
Andreas Poehlmann <andreas@poehlmann.io>
2021-10-18 06:54:01 +0200

    intro

--- talk/__init__.py
+++ talk/__init__.py
@@ -1,5 +1,7 @@
+"""
+just a few definitions to make the introduction slide more fun
+"""
 from collections import namedtuple
-
 
 Degree = type("Degree", (), dict(__init__=lambda *_: None))
 Career = type("Career", (), dict(__init__=lambda *_: None))
