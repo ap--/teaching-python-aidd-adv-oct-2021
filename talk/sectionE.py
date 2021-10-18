19d47a72d27123d0a82d6c7a3f9dca4632e0579d
Andreas Poehlmann <andreas@poehlmann.io>
2021-10-18 10:29:50 +0200

    adding sections

---
+++ talk/sectionE.py
@@ -0,0 +1,17 @@
+"""                   .::::::::::.
+                    .::``::::::::::.
+                    :::..:::::::::::
+                    ````````::::::::
+            .::::::::::::::::::::::: iiiiiii,
+         .:::::::::::::::::::::::::: iiiiiiiii.
+         ::::::                          iiiiii
+         ::::::    THANK YOU FOR         iiiiii
+         ::::::      YOUR ATTENTION!     iiiiii
+         ::::::                          iiiiii
+         `::::::::: iiiiiiiiiiiiiiiiiiiiiiiiii`
+            `:::::: iiiiiiiiiiiiiiiiiiiiiii`
+                    iiiiiiii,,,,,,,,
+                    iiiiiiiiiii''iii
+                    `iiiiiiiiii..ii`
+                      `iiiiiiiiii`
+"""
